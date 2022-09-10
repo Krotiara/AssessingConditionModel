bfbb81b5f876ea94a6ca35b8bd8fae70417730ec
Lisovenko Anton <70dog@mail.ru>
2022-09-10 21:38:47 +0500

    Конструктор параметра пациента

--- InfluenceCalculator.API/Models/PatientParameter.cs
+++ InfluenceCalculator.API/Models/PatientParameter.cs
@@ -4,9 +4,19 @@ namespace InfluenceCalculator.API.Models
 {
     public class PatientParameter : IPatientParameter
     {
+        public PatientParameter(string name, object value,
+            int positiveDynamicCoef = 1, object? dynamicValue = null)
+        {
+            Name = name;
+            Value = value;
+            PositiveDynamicCoef = positiveDynamicCoef;
+            DynamicValue = dynamicValue;
+        }
+
         public string Name { get; set; }
         public object Value { get; set; }
-        public object DynamicValue { get; set; }
+        public object? DynamicValue { get; set; }
+
         public int PositiveDynamicCoef { get; set; } = 1;
     }
 }
