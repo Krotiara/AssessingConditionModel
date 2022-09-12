d998b193913c7490daa35edcb530d03d0c9d5173
itemBimIT <70dog@mail.ru>
2022-09-12 09:24:37 +0500

    проект микро сервиса обработки сырых данных пользователя

---
+++ PatientDataHandler.API/Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "ћикросервис обработки данных пациентов",
+        Description = "ћикросервис отвечает за обработку сырых данных пациента.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Name = "Lisovenko Anton",
+            Email = String.Empty
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Patient Data Handler API");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
