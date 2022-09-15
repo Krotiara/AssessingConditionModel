ee33e1099aebc3c9c05037bf634deb590d3714e3
itemBimIT <70dog@mail.ru>
2022-09-15 10:06:58 +0500

    Интерфейс сущности пациента

---
+++ Interfaces/IPatient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IPatient
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public long MedicalHistoryNumber { get; set; }
+    }
+}
