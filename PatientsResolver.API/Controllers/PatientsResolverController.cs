3909279b0a3e7b26bfbb36749c59359491a068b6
Lisovenko Anton <70dog@mail.ru>
2022-09-18 15:14:58 +0500

    Закоментирован ненужный Save метод онтроллера

--- PatientsResolver.API/Controllers/PatientsResolverController.cs
+++ PatientsResolver.API/Controllers/PatientsResolverController.cs
@@ -26,7 +26,7 @@ namespace PatientsResolver.API.Controllers
         {
             try
             {
-                return Ok(await mediator.Send(new GetPatientQuery() { PatientId = patientId}));
+                return Ok(await mediator.Send(new GetPatientDataQuery() { PatientId = patientId}));
             }
             catch(Exception ex)
             {
@@ -90,58 +90,58 @@ namespace PatientsResolver.API.Controllers
         //}
 
 
-        [HttpPost("saveData")]
-        public async Task<ActionResult<List<IPatientData>>> SavePatientDataAsync(List<IPatientData> patientDatas)
-        {
-            throw new NotImplementedException();
-//            try
-//            {
-//#warning могут быть проблемы с кастами.
-//                List<PatientData> datas = await mediator.Send(new CreatePatientDatasCommand()
-//                {
-//                    PatientDatas = patientDatas.Cast<PatientData>().ToList()
-//                });
-//                return datas.Cast<IPatientData>().ToList();
-//            }
-//            catch(Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-            //try
-            //{
-            //    using (var transaction = patientsDataDbContext.Database.BeginTransaction())
-            //    {
-            //        //Рассмотреть необходимость сужения try catch до поэлементного отлова.
-            //        try
-            //        {
-            //            foreach (PatientData data in patientDatas)
-            //            {
-            //                //Не уверен, что upcast хороший выбор.
-            //                await patientsDataDbContext.PatientsParameters.AddRangeAsync(data.Parameters.Cast<PatientParameter>());
-            //                await patientsDataDbContext.PatientDatas.AddAsync(data);
-            //                await patientsDataDbContext.SaveChangesAsync();
-            //            }
+//        [HttpPost("saveData")]
+//        public async Task<ActionResult<List<IPatientData>>> SavePatientDataAsync(List<IPatientData> patientDatas)
+//        {
+//            throw new NotImplementedException();
+////            try
+////            {
+////#warning могут быть проблемы с кастами.
+////                List<PatientData> datas = await mediator.Send(new CreatePatientDatasCommand()
+////                {
+////                    PatientDatas = patientDatas.Cast<PatientData>().ToList()
+////                });
+////                return datas.Cast<IPatientData>().ToList();
+////            }
+////            catch(Exception ex)
+////            {
+////                return BadRequest(ex.Message);
+////            }
+//            //try
+//            //{
+//            //    using (var transaction = patientsDataDbContext.Database.BeginTransaction())
+//            //    {
+//            //        //Рассмотреть необходимость сужения try catch до поэлементного отлова.
+//            //        try
+//            //        {
+//            //            foreach (PatientData data in patientDatas)
+//            //            {
+//            //                //Не уверен, что upcast хороший выбор.
+//            //                await patientsDataDbContext.PatientsParameters.AddRangeAsync(data.Parameters.Cast<PatientParameter>());
+//            //                await patientsDataDbContext.PatientDatas.AddAsync(data);
+//            //                await patientsDataDbContext.SaveChangesAsync();
+//            //            }
 
-            //            await patientsDataDbContext.SaveChangesAsync();
-            //            transaction.Commit();
-            //        }
-            //        catch (Exception ex) //TODO осмысленный try catch
-            //        {
-            //            //TODO add log
-            //            transaction.Rollback();
-            //            return BadRequest(ex.Message);
-            //        }
-            //    }
+//            //            await patientsDataDbContext.SaveChangesAsync();
+//            //            transaction.Commit();
+//            //        }
+//            //        catch (Exception ex) //TODO осмысленный try catch
+//            //        {
+//            //            //TODO add log
+//            //            transaction.Rollback();
+//            //            return BadRequest(ex.Message);
+//            //        }
+//            //    }
 
-            //    foreach (IPatientData patientData in patientDatas)
-            //        await mediator.Send(new UpdatePatientDataCommand() { PatientData = patientData });
+//            //    foreach (IPatientData patientData in patientDatas)
+//            //        await mediator.Send(new UpdatePatientDataCommand() { PatientData = patientData });
 
-            //    return Ok();
-            //}
-            //catch(Exception ex)
-            //{
-            //    return BadRequest(ex.Message);
-            //}
-        }
+//            //    return Ok();
+//            //}
+//            //catch(Exception ex)
+//            //{
+//            //    return BadRequest(ex.Message);
+//            //}
+//        }
     }
 }
