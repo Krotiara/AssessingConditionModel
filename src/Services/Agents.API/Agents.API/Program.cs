9b6ab9d2e7719618048e82a44969180d4e5127e2
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-10-12 19:30:46 +0500

    Головной проект для микросервиса agents.api

---
+++ src/Services/Agents.API/Agents.API/Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "Микросервис агентов системы",
+        Description = "Отвечает за отслеживание и прогнозирование состояния пациентов. " +
+        "\r\nОтслеживание также подразумевает под собой оповещение об изменении " +
+        "состояния пациента путем сообщений пользователю врачу." +
+        "\r\nНа программном уровне отвечает за работу агентов системы.\r\n.",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Name = "Lisovenko Anton",
+            Email = String.Empty
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Patient Data Handler API");
+    c.RoutePrefix = string.Empty;
+});
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
