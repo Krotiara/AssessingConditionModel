8d3b5e6b469dff499876f7034756554431877b3f
Lisovenko Anton <70dog@mail.ru>
2022-10-17 11:26:16 +0500

    Update PatientsResolverController.cs

--- src/Services/PatientsResolver.API/PatientsResolver.API/Controllers/PatientsResolverController.cs
+++ src/Services/PatientsResolver.API/PatientsResolver.API/Controllers/PatientsResolverController.cs
@@ -24,6 +24,7 @@ namespace PatientsResolver.API.Controllers
         }
 
 
+#warning для тестируемых нужд
         [HttpGet("update/{patientId}")]
         public async Task<ActionResult> UpdatePatient(int patientId)
         {
