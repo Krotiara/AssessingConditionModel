c0656220ce3e85aa639af8b0db69a0d53f9a3764
Lisovenko Anton <70dog@mail.ru>
2022-10-19 18:46:59 +0500

    Попытка запустить onnx модель

--- src/Services/BioAge.API/BioAge.API.Service/Command/CalculateBioAgeByFunctionalParamsCommandHandler.cs
+++ src/Services/BioAge.API/BioAge.API.Service/Command/CalculateBioAgeByFunctionalParamsCommandHandler.cs
@@ -6,34 +6,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using CNTK;
 
 namespace BioAge.API.Service.Command
 {
+
+    //public class TestInput
+    //{
+    //    [VectorType(10)]
+    //    [ColumnName("input.1")]
+    //    public float[] Features { get; set; }
+    //}
+
     public class CalculateBioAgeByFunctionalParamsCommandHandler : IRequestHandler<CalculateBioAgeByFunctionalParamsCommand, double>
     {
+
+        public CalculateBioAgeByFunctionalParamsCommandHandler()
+        {
+        }
+
         public async Task<double> Handle(CalculateBioAgeByFunctionalParamsCommand request, CancellationToken cancellationToken)
         {
             try
             {
                 Dictionary<ParameterNames, PatientParameter> paramsDict = request.BioAgeCalculationParameters.Parameters;
-                double systolicPressure = double.Parse(
+                float systolicPressure = float.Parse(
                     paramsDict[ParameterNames.SystolicPressure].Value.Replace(",","."));
-                double diastolicPressure = double.Parse(
+                float diastolicPressure = float.Parse(
                     paramsDict[ParameterNames.DiastolicPressure].Value.Replace(",", "."));
-                double inhaleBreathHolding = double.Parse(
+                float inhaleBreathHolding = float.Parse(
                     paramsDict[ParameterNames.InhaleBreathHolding].Value.Replace(",", "."));
-                double outhaleBreathHolding = double.Parse(
+                float outhaleBreathHolding = float.Parse(
                     paramsDict[ParameterNames.OuthaleBreathHolding].Value.Replace(",", "."));
-                double lungCapacity = double.Parse(
+                float lungCapacity = float.Parse(
                     paramsDict[ParameterNames.LungCapacity].Value.Replace(",", "."));
-                double weight = double.Parse(
+                float weight = float.Parse(
                     paramsDict[ParameterNames.Weight].Value.Replace(",", "."));
-                double accommodation = double.Parse(
+                float accommodation = float.Parse(
                     paramsDict[ParameterNames.Accommodation].Value.Replace(",", "."));
-                double hearingAcuity = double.Parse(
+                float hearingAcuity = float.Parse(
                     paramsDict[ParameterNames.HearingAcuity].Value.Replace(",", "."));
-                double staticBalancing = double.Parse(
+                float staticBalancing = float.Parse(
                     paramsDict[ParameterNames.StaticBalancing].Value.Replace(",", "."));
+
+                //string modelPath =Path.Combine(Directory.GetCurrentDirectory(), @"Resources/bioAgeFuncModel.onnx");
+
+                //var a = DeviceDescriptor.AllDevices();
+                //var deviceDescriptor = DeviceDescriptor.AllDevices().First();
+                //var function = Function.Load("modelPath", deviceDescriptor, ModelFormat.ONNX);
+
+                //var data = mlContext.Data.LoadFromEnumerable(new List<TestInput>());
+                //var pipeline = mlContext.Transforms ApplyOnnxModel(modelFile: modelPath, outputColumnNames: new[] { "bioAge"}, inputColumnNames: new[] { "input.1" });
+                //// Fit scoring pipeline
+                //var model = pipeline.Fit(data);
+
+
+                //var session = new InferenceSession(modelPath);
+
+                //float[] input = { systolicPressure, diastolicPressure, 
+                //    (systolicPressure - diastolicPressure), inhaleBreathHolding, 
+                //    outhaleBreathHolding, lungCapacity, weight, accommodation, 
+                //    hearingAcuity, staticBalancing };
+
+
+
+                //Tensor<float> t1 = new DenseTensor<float>(input, new int[] {1,10});
+               
+                //var inputs = new List<NamedOnnxValue>() {NamedOnnxValue.CreateFromTensor<float>(null, t1)};
+                //using(var outPut  = session.Run(inputs))
+                //{
+                //    var a = 1;
+                //}
+
                 return await Task.FromResult<double>(
                    - 1.07 * systolicPressure
                    + 1.1 * diastolicPressure
