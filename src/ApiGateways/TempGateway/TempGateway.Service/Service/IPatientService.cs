d1400a913907ea0414569e745207e558e7cdeba6
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-10-22 21:00:27 +0500

    Сервис для шлюза апи по отношению к пациентам

---
+++ src/ApiGateways/TempGateway/TempGateway.Service/Service/IPatientService.cs
@@ -0,0 +1,18 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempGateway.Service.Service
+{
+    public interface IPatientService
+    {
+        public Task<IPatient> GetPatientById(int id);
+
+        public Task<IAgingPatientState> GetAgingPatientStateByPatientId(int patientId);
+
+        public Task<IList<IAgingPatientState>> GetAgingDynamicsByPatientId(int patientId);
+    }
+}
--- src/ApiGateways/TempGateway/TempGateway/Controllers/PatientsController.cs
+++ src/ApiGateways/TempGateway/TempGateway/Controllers/PatientsController.cs
@@ -1,37 +1,49 @@
 ﻿using Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TempGateway.Service.Service;
 
 namespace TempGateway.Controllers
 {
     public class PatientsController: Controller
     {
         private readonly IMediator mediator;
+        private readonly IPatientService patientService;
 
-        public PatientsController(IMediator mediator)
+        public PatientsController(IMediator mediator, IPatientService patientService)
         {
             this.mediator = mediator;
+            this.patientService = patientService;
         }
 
 
         [HttpGet("patients/{patientId}")]
         public async Task<ActionResult<IPatient>> GetPatient(int patientId)
         {
-            throw new NotImplementedException();
+            IPatient patient = await patientService.GetPatientById(patientId);
+            if(patient == null)
+                return BadRequest($"No patient found for id {patientId}");
+            return Ok(patient);
         }
 
 
         [HttpGet("agingState/{patientId}")]
         public async Task<ActionResult<IAgingPatientState>> GetPatientAgingState(int patientId)
         {
-            throw new NotImplementedException();
+            IAgingPatientState agingState = await patientService.GetAgingPatientStateByPatientId(patientId);
+            if (agingState == null)
+                return BadRequest($"No aging patient state found for id {patientId}");
+            return Ok(agingState);
         }
 
 
         [HttpPost("agingDynamics/{patientId}")]
-        public async Task<ActionResult<List<IAgingPatientState>>> GetPatientAgingDynamics(int patientId, [FromBody] DateTime[] timeSpan)
+        public async Task<ActionResult<IList<IAgingPatientState>>> GetPatientAgingDynamics(int patientId, [FromBody] DateTime[] timeSpan)
         {
-            throw new NotImplementedException();
+            IList<IAgingPatientState> agingPatientStates = await patientService.GetAgingDynamicsByPatientId(patientId);
+            if(agingPatientStates == null)
+                return BadRequest($"No aging patient states found for id {patientId}");
+            return Ok(agingPatientStates);
         }
 
 
