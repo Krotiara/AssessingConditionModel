cb8fa5e5b805c49246dac23574a0906b4b1b3205
Lisovenko Anton <70dog@mail.ru>
2022-10-26 16:29:08 +0500

    Преобразование Enum в текстовое описание, косметика UI

---
+++ src/Web/WebMVC/Models/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace WebMVC.Models
+{
+    public static class Extensions
+    {
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an `Enum`.
+        /// </summary>
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+                where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+
+
+        public static string GetDisplayAttributeValue(this Enum enumValue)
+        {
+            return enumValue.GetAttribute<DisplayAttribute>().Name;
+        }
+    }
+}
