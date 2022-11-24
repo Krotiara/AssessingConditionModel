9f8a403f2b9dd29b1e9674cd7c121855455a4808
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-11-24 11:37:36 +0500

    Инициализирующие поля в тестах query

--- src/Services/PatientsResolver.API/Tests/PatientsResolver.API.UnitTests/Query/GetInfluencesQueryTests.cs
+++ src/Services/PatientsResolver.API/Tests/PatientsResolver.API.UnitTests/Query/GetInfluencesQueryTests.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PatientsResolver.API.Data.Repository;
+using PatientsResolver.API.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace PatientsResolver.API.UnitTests.Query
 {
     public class GetInfluencesQueryTests
     {
+        PatientsDataDbContext dbContext;
+        CancellationToken token;
+        InfluenceRepository rep;
+
+        public GetInfluencesQueryTests()
+        {
+            var options = new DbContextOptionsBuilder<PatientsDataDbContext>()
+               .UseInMemoryDatabase(databaseName: "test")
+               .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+               .Options;
+            // set delay time after which the CancellationToken will be canceled
+            var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+            dbContext = new PatientsDataDbContext(options);
+            token = cancellationTokenSource.Token;
+            rep = new InfluenceRepository(dbContext);
+        }
 
         [Fact]
         public void GetInfluencesMustSetPatientProperty()
