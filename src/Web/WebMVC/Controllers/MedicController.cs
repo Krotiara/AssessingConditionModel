a9311a729f5d8dc0fbe8e03637cca6c6be5a1ed0
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-12-18 11:51:04 +0500

    Начальная страница для кабинета врача

---
+++ src/Web/WebMVC/Controllers/MedicController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class MedicController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPatientsDataView()
+        {
+            return View("~/Views/MedicPersonalPage/PatientsDataView.cshtml");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetDataInputView()
+        {
+            return View("~/Views/MedicPersonalPage/DataInputView.cshtml");
+        }
+    }
+}
