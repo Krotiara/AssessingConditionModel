d1fef539b6d747b035a13523df2353468b0b9da4
Krotiara <33466897+Krotiara@users.noreply.github.com>
2022-12-19 09:21:21 +0500

    Страница статистики, туда перенесено получение общей динамики биовозраста,  мелкий рефакторинг

--- src/Web/WebMVC/Controllers/MedicController.cs
+++ src/Web/WebMVC/Controllers/MedicController.cs
@@ -5,19 +5,16 @@ namespace WebMVC.Controllers
     public class MedicController : Controller
     {
         [HttpGet("data")]
-        public async Task<IActionResult> GetPatientsDataView()
-        {
-#warning Почему-то загружает второй раз при GetPartialViewByTag, поэтому теряются данные
-            return View("~/Views/MedicPersonalPage/PatientsDataView.cshtml");
-        }
+        public async Task<IActionResult> GetPatientsDataView() => View("~/Views/MedicPersonalPage/PatientsDataView.cshtml");
 
 
         [HttpGet("dataInput")]
-        public async Task<IActionResult> GetDataInputView()
-        {
-            return View("~/Views/MedicPersonalPage/DataInputView.cshtml");
-        }
+        public async Task<IActionResult> GetDataInputView() => View("~/Views/MedicPersonalPage/DataInputView.cshtml");
 
+
+        [HttpGet("statistics")]
+        public async Task<IActionResult> GetStatisticsView() => View("~/Views/MedicPersonalPage/StatisticsView.cshtml");
+        
 
         [HttpGet]
         public async Task<IActionResult> GetPartialViewByTag(string tag)
@@ -26,6 +23,8 @@ namespace WebMVC.Controllers
                 return PartialView("~/Views/Patient/_SearchPatientView.cshtml");
             else if (tag == "add")
                 return PartialView("~/Views/Patient/_AddPatientView.cshtml", new Models.Patient());
+            else if (tag == "commonBioAgeDynamic")
+                return PartialView("~/Views/Patient/_CommonAgingDynamicsStartView.cshtml");
             else throw new NotImplementedException();
         }
     }
