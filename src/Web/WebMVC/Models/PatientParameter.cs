ae5b8d80a84588f3fdbc788bddbc413824d77298
Lisovenko Anton <70dog@mail.ru>
2022-12-26 21:57:49 +0500

    Корректировка видов добавления воздействия и показателей пациента

--- src/Web/WebMVC/Models/PatientParameter.cs
+++ src/Web/WebMVC/Models/PatientParameter.cs
@@ -1,4 +1,5 @@
 ﻿using Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebMVC.Models
 {
@@ -8,11 +9,17 @@ namespace WebMVC.Models
 
         public int Id { get; set; }
         public int InfluenceId { get; set; }
+
+        [Display(Name="Идентификатор пациента")]
         public int PatientId { get; set; }
+        [Display(Name = "Временная отметка")]
         public DateTime Timestamp { get; set; }
+        [Display(Name = "Имя показателя")]
         public ParameterNames ParameterName { get; set; }
         public string NameTextDescription { get; set; }
+        [Display(Name = "Значение")]
         public string Value { get; set; }
+        [Display(Name = "Вторичный")]
         public bool IsDynamic { get; set; }
         public int PositiveDynamicCoef { get; set; }
     }
