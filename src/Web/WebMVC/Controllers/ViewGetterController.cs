e578d07ca395b9f21b52bae0254d48c0b61f05a2
Lisovenko Anton <70dog@mail.ru>
2023-01-09 11:23:16 +0500

    Шаблон с меню для страницы врача

--- src/Web/WebMVC/Controllers/ViewGetterController.cs
+++ src/Web/WebMVC/Controllers/ViewGetterController.cs
@@ -9,15 +9,15 @@ namespace WebMVC.Controllers
         public async Task<IActionResult> GetPartialViewByTag(string tag)
         {
             if (tag == "search")
-                return PartialView("~/Views/Patient/SearchPatientView.cshtml");
+                return View("~/Views/Patient/SearchPatientView.cshtml");
             else if (tag == "commonBioAgeDynamic")
-                return PartialView("~/Views/Patient/CommonAgingDynamicsStartView.cshtml");
+                return View("~/Views/Patient/CommonAgingDynamicsStartView.cshtml");
             else if (tag == "addPatient")
-                return PartialView("~/Views/DataInputPartialViews/AddPatientView.cshtml", new Patient());
+                return View("~/Views/DataInputPartialViews/AddPatientView.cshtml", new Patient());
             else if (tag == "addInfluence")
-                return PartialView("~/Views/DataInputPartialViews/AddInfluenceView.cshtml", new InfluenceViewFormat());
+                return View("~/Views/DataInputPartialViews/AddInfluenceView.cshtml", new InfluenceViewFormat());
             else if (tag == "addFile")
-                return PartialView("~/Views/DataInputPartialViews/AddDataFileView.cshtml");
+                return View("~/Views/DataInputPartialViews/AddDataFileView.cshtml");
             else throw new NotImplementedException();
         }
     }
