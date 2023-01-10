3e626f7a256d4b7d6704fac2c78eb39c47083bf8
Lisovenko Anton <70dog@mail.ru>
2023-01-10 18:33:13 +0500

    корректировка формы внесения воздействия

--- src/Web/WebMVC/Models/PatientParameter.cs
+++ src/Web/WebMVC/Models/PatientParameter.cs
@@ -12,18 +12,25 @@ namespace WebMVC.Models
 
         [Display(Name="Идентификатор пациента")]
         public int PatientId { get; set; }
+        
         [Display(Name = "Временная отметка")]
+        [DataType(DataType.DateTime)]
 #warning Нужна валидация
         public DateTime Timestamp { get; set; }
+        
         [Display(Name = "Имя показателя")]
         [ParamNameSet(ErrorMessage = "Не указано наименование параметра")]
         public ParameterNames ParameterName { get; set; }
+       
         public string NameTextDescription { get; set; }
+        
         [Display(Name = "Значение")]
         [Required(ErrorMessage = "Не указано значение параметра")]
         public string Value { get; set; }
+        
         [Display(Name = "Вторичный")]
         public bool IsDynamic { get; set; }
+       
         public int PositiveDynamicCoef { get; set; }
     }
 }
