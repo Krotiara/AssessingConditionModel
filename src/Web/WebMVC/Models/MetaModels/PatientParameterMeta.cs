b1bff59f653c5dd19a8635c7faa5c8b6c78c05d6
Lisovenko Anton <70dog@mail.ru>
2023-01-11 13:48:00 +0500

    Пустой сервис для получения мета информации о параметрах

---
+++ src/Web/WebMVC/Models/MetaModels/PatientParameterMeta.cs
@@ -0,0 +1,6 @@
+﻿using Interfaces;
+
+namespace WebMVC.Models.MetaModels
+{
+    public record ParameterMeta(ParameterNames param, Func<string, bool> isValidValue);
+}
---
+++ src/Web/WebMVC/Services/IParametersMetaService.cs
@@ -0,0 +1,10 @@
+﻿using Interfaces;
+using WebMVC.Models.MetaModels;
+
+namespace WebMVC.Services
+{
+    public interface IParametersMetaService
+    {
+        public ParameterMeta GetParameterMeta(ParameterNames paramName);
+    }
+}
