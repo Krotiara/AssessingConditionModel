ac7e5eabccaf4062af7f2bac11110c592f1987fd
Krotiara <33466897+Krotiara@users.noreply.github.com>
2023-02-12 14:40:10 +0500

    рокинул throw new NotImplementedException(); //TODO рефакторинг в связи с новым API

--- src/Services/Agents.API/Agents.API.Service/Command/AddAgingStateCommandHandler.cs
+++ src/Services/Agents.API/Agents.API.Service/Command/AddAgingStateCommandHandler.cs
@@ -11,17 +11,18 @@ namespace Agents.API.Service.Command
 {
     public class AddAgingStateCommandHandler : IRequestHandler<AddAgingStateCommand, AgingState>
     {
-        private readonly IAgingStatesRepository agingStatesRepository;
+        //private readonly IAgingStatesRepository agingStatesRepository;
 
 
-        public AddAgingStateCommandHandler(IAgingStatesRepository agingStatesRepository)
+        public AddAgingStateCommandHandler()
         {
-            this.agingStatesRepository = agingStatesRepository;
+            //this.agingStatesRepository = agingStatesRepository;
         }
 
         public async Task<AgingState> Handle(AddAgingStateCommand request, CancellationToken cancellationToken)
         {
-            return await agingStatesRepository.AddState(request.AgingStateToAdd, false); //TODO try catch
+            throw new NotImplementedException(); //TODO - рефакторинг всвязи с новым API
+            //return await agingStatesRepository.AddState(request.AgingStateToAdd, false); //TODO try catch
         }
     }
 }
