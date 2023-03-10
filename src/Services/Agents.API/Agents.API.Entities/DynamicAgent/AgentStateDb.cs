b1404553533b6ea470979a764cb3ab6828da5292
Lisovenko Anton <70dog@mail.ru>
2023-03-10 08:59:14 +0500

    Agent state db entity for saving into mongo

---
+++ src/Services/Agents.API/Agents.API.Entities/DynamicAgent/AgentStateDb.cs
@@ -0,0 +1,22 @@
+﻿using Interfaces;
+using Interfaces.DynamicAgent;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Agents.API.Entities.DynamicAgent
+{
+    internal class AgentStateDb : IAgentState
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string AgentId { get; set; }
+
+        public AgentType AgentType { get; set; }
+
+        public string Name { get; set; }
+        public double NumericCharacteristic { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
