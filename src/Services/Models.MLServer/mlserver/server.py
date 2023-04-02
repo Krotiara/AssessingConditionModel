7ba8417e90afa8f45891096c7ca6b63c2d48acbf
Lisovenko Anton <70dog@mail.ru>
2023-04-02 22:45:08 +0500

    ml server ports

--- src/Services/Models.MLServer/mlserver/server.py
+++ src/Services/Models.MLServer/mlserver/server.py
@@ -96,4 +96,4 @@ def index():
 
 if __name__ == '__main__':
     metas = db_connection.session.query(ModelMeta).all()
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=80, host='0.0.0.0', debug=True)
