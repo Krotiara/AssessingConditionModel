dcef48598b4374e91bb6dee4267654f46cf3b3dd
Lisovenko Anton <70dog@mail.ru>
2023-04-25 09:00:40 +0500

    remove no ness add

--- src/Services/Agents.API/Agents.API/Program.cs
+++ src/Services/Agents.API/Agents.API/Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers();
 
 //builder.Services.AddHttpsRedirection(options =>
 //{
