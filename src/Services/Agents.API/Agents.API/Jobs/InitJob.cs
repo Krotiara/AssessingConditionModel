eb9debc4e85dc206a32c954660f87229927228ae
Lisovenko Anton <70dog@mail.ru>
2023-05-04 16:26:52 +0500

    Create InitJob.cs

---
+++ src/Services/Agents.API/Agents.API/Jobs/InitJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace Agents.API.Jobs
+{
+    public class InitJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
