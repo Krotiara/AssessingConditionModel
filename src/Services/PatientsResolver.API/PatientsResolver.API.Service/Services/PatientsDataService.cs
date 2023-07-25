60fd9bfb0b5c5dba11cf59f9cbc2c94bf923c617
Lisovenko Anton <70dog@mail.ru>
2023-07-25 13:17:48 +0500

    rename Interfaces to ASMLib

---
+++ src/Services/PatientsResolver.API/PatientsResolver.API.Service/Services/PatientsDataService.cs
@@ -0,0 +1,23 @@
+﻿using PatientsResolver.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientsResolver.API.Service.Services
+{
+    public class PatientsDataService
+    {
+        public async Task Insert(IEnumerable<Influence> influences)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task Insert(IEnumerable<Patient> patients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
