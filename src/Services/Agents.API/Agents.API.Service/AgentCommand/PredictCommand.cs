d1c83b46b947d7fcf175bdbc38bf10f6b740b46c
Lisovenko Anton <70dog@mail.ru>
2023-08-27 12:05:03 +0500

    predict command

--- src/Services/Agents.API/Agents.API.Service/AgentCommand/PredictCommand.cs
+++ src/Services/Agents.API/Agents.API.Service/AgentCommand/PredictCommand.cs
@@ -4,7 +4,6 @@ using Agents.API.Entities.Requests;
 using Agents.API.Service.Services;
 using Interfaces;
 using Interfaces.DynamicAgent;
-using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -14,51 +13,70 @@ using System.Threading.Tasks;
 
 namespace Agents.API.Service.AgentCommand
 {
-    public class GetDentistSumCommand : IAgentCommand
+    public class PredictCommand : IAgentCommand
     {
         private readonly PredictionRequestsService _pMService;
         private readonly PatientsService _pPSerivce;
-        private readonly string _modelsServerUrl;
-        private readonly EnvSettings _settings;
-
         private readonly string _ageParameter = "Age"; //TODO вынести в настройки. 
+
+        public PredictCommand(PredictionRequestsService pMService, PatientsService requestService)
+        {
+            _pMService = pMService;
+            _pPSerivce = requestService;
+        }
 
         public ConcurrentDictionary<string, IProperty> Variables { get; set; }
         public ConcurrentDictionary<string, IProperty> Properties { get; set; }
         public IAgentPropertiesNamesSettings PropertiesNamesSettings { get; set; }
 
-
-        public GetDentistSumCommand(PredictionRequestsService pMService,
-            PatientsService pPSerivce, 
-            IOptions<EnvSettings> settings)
-        {
-            _pMService = pMService;
-            _pPSerivce = pPSerivce;
-            _modelsServerUrl = settings.Value.ModelsApiUrl;
-            _settings = settings.Value;
-        }
-
         public Delegate Command => async (int age) =>
         {
+            if (!CheckCommand())
+                throw new ExecuteCommandException($"No requered args in PredictCommand.");
+
             string patientId = Properties[PropertiesNamesSettings.Id].Value as string;
             string patientAffiliation = Properties[PropertiesNamesSettings.Affiliation].Value as string;
             DateTime endTimestamp = (DateTime)Variables[PropertiesNamesSettings.EndTimestamp].Value;
             string mlModelId = (string)Variables[PropertiesNamesSettings.MlModel].Value;
 
-            if (mlModelId == null)
-                throw new ExecuteCommandException($"Cannot resolve model key");
-
             var meta = await _pMService.Get(mlModelId);
             if (meta == null)
                 throw new ExecuteCommandException($"No meta for model id {mlModelId}");
-            List<string> names = meta.ParamsNamesList;
-            PatientParametersRequest request = new(patientAffiliation, patientId, endTimestamp, names);
+
+            PatientParametersRequest request = new(patientAffiliation, patientId, endTimestamp, meta.ParamsNamesList);
             Dictionary<string, Parameter> parameters = await _pPSerivce.GetPatientParameters(request);
 
             if (parameters == null)
-                throw new ExecuteCommandException($"Cannot get latest parameters for patient {patientId}:{patientAffiliation}. See logs.");
+                throw new ExecuteCommandException($"Cannot get latest parameters for patient {patientId}:{patientAffiliation}.");
 
+            double[] args = GetInputArgs(parameters, meta.ParamsNamesList, age);
+
+            var responce = await _pMService.Predict(mlModelId, args);
+            if (responce == null)
+                throw new ExecuteCommandException("Cannot get answer");
+
+            if (!responce.IsSuccessStatusCode)
+                throw new ExecuteCommandException($"{responce.StatusCode}:{responce.ReasonPhrase}");
+            else
+            {
+                var res = await responce.DeserializeBody<float[]>();
+                return res.First();
+            }
+        };
+
+
+        private bool CheckCommand() => Properties.ContainsKey(PropertiesNamesSettings.Id)
+            && Properties.ContainsKey(PropertiesNamesSettings.Affiliation)
+            && Variables.TryGetValue(PropertiesNamesSettings.EndTimestamp, out IProperty p)
+            && p.Value is DateTime
+            && Variables.TryGetValue(PropertiesNamesSettings.MlModel, out IProperty ml)
+            && ml.Value is string;
+
+
+        private double[] GetInputArgs(Dictionary<string, Parameter> parameters, List<string> names, int age)
+        {
             double[] inputArgs = new double[names.Count];
+
             for (int i = 0; i < names.Count; i++)
             {
                 if (Variables.ContainsKey(names[i]) && Variables[names[i]].Value != null)
@@ -79,24 +97,14 @@ namespace Agents.API.Service.AgentCommand
                     continue;
                 }
 
-                
+
                 if (!parameters.ContainsKey(names[i]))
                     throw new ExecuteCommandException($"One of the required parameters is not found: {names[i]}");
+
                 inputArgs[i] = parameters[names[i]].Value;
-                
             }
 
-            var responce = await _pMService.Predict(mlModelId, inputArgs);
-            if (responce == null)
-                throw new ExecuteCommandException("Cannot get answer");
-
-            if (!responce.IsSuccessStatusCode)
-                throw new ExecuteCommandException($"{responce.StatusCode}:{responce.ReasonPhrase}");
-            else
-            {
-                var res = await responce.DeserializeBody<float[]>();
-                return (int)Math.Ceiling(res.First());
-            }
-        };
+            return inputArgs;
+        }
     }
 }
