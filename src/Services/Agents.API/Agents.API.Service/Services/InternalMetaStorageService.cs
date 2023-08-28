c7c745cb900623dcf44316736958653fc533ad72
Lisovenko Anton <70dog@mail.ru>
2023-08-28 08:07:58 +0500

    Update InternalMetaStorageService.cs

--- src/Services/Agents.API/Agents.API.Service/Services/InternalMetaStorageService.cs
+++ src/Services/Agents.API/Agents.API.Service/Services/InternalMetaStorageService.cs
@@ -22,7 +22,7 @@ namespace Agents.API.Service.Services
                 {SystemCommands.GetAge,
                     new CommandArgsTypesMeta(new List<(Type, string)> {(typeof(DateTime), "timestamp") }, typeof(int)) },
                 {SystemCommands.Predict,
-                    new CommandArgsTypesMeta(new List<(Type, string)> {(typeof(int), "age")}, typeof(float[]))}, 
+                    new CommandArgsTypesMeta(new List<(Type, string)> {(typeof(int), "age")}, typeof(float[]))},
                 {SystemCommands.GetInfluences,
                     new CommandArgsTypesMeta(new List<(Type, string)> {}, typeof(List<Influence>))}
             };
