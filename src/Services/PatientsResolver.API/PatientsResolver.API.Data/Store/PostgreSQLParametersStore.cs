74f62791ad55742eead0dd3fa9835b79ef3190f0
Krotiara <70dog@mail.ru>
2024-02-17 18:05:11 +0500

    Update PostgreSQLParametersStore.cs

--- src/Services/PatientsResolver.API/PatientsResolver.API.Data/Store/PostgreSQLParametersStore.cs
+++ src/Services/PatientsResolver.API/PatientsResolver.API.Data/Store/PostgreSQLParametersStore.cs
@@ -1,10 +1,4 @@
-﻿using Interfaces;
-using PatientsResolver.API.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PatientsResolver.API.Entities;
 
 namespace PatientsResolver.API.Data.Store
 {
