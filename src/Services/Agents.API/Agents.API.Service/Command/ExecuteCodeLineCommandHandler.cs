ba65d0b9d15458d126323d9ff33f1cd3f33b11b5
Krotiara <70dog@mail.ru>
2024-02-22 10:06:06 +0500

    Update ExecuteCodeLineCommandHandler.cs

--- src/Services/Agents.API/Agents.API.Service/Command/ExecuteCodeLineCommandHandler.cs
+++ src/Services/Agents.API/Agents.API.Service/Command/ExecuteCodeLineCommandHandler.cs
@@ -1,21 +1,13 @@
 ﻿using Agents.API.Entities;
-using Agents.API.Entities.AgentsSettings;
 using Agents.API.Interfaces;
 using Interfaces;
 using Interfaces.DynamicAgent;
 using MediatR;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
-using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Agents.API.Service.Command
 {
